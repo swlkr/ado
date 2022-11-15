9bca550bb50febe20e315ad42bb52f7b812a7a00
Sean Walker <swlkr.rbl@gmail.com>
2022-11-15 11:31:34 -0700

    Read or create a TODO.txt file in the current dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::OpenOptions;
+use std::io::{Read, Result};
+
+const FILENAME: &str = "./TODO.txt";
+
+fn main() -> Result<()> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(FILENAME)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("{}", contents);
+
+    Ok(())
 }
