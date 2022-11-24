4eee13e689ba200b3c2dd16edf6bc23619bb2cfc
Sean Walker <swlkr.rbl@gmail.com>
2022-11-23 21:45:26 -0700

    Functional-ish core, imperative shell

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ enum Action {
     List,
     Delete,
     Undo,
+    Help,
 }
 
 impl FromStr for Action {
@@ -23,11 +24,37 @@ impl FromStr for Action {
             "list" => Ok(Action::List),
             "done" => Ok(Action::Done),
             "add" => Ok(Action::Add),
-            "del" => Ok(Action::Delete),
+            "delete" => Ok(Action::Delete),
             "undo" => Ok(Action::Undo),
             _ => Err(String::from("unsupported action")),
         }
     }
+}
+
+fn file() -> std::io::Result<File> {
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(FILENAME)
+}
+
+fn read_file() -> std::io::Result<String> {
+    let mut contents = String::new();
+    file()?.read_to_string(&mut contents)?;
+    return Ok(contents);
+}
+
+fn write_file(todos: Vec<Todo>) -> std::io::Result<String> {
+    let contents = todos
+        .iter()
+        .map(|todo| todo.to_s())
+        .collect::<Vec<String>>()
+        .join("\n");
+    let mut f = file()?;
+    f.set_len(0)?;
+    f.write_all(contents.as_bytes())?;
+    return Ok(contents);
 }
 
 #[derive(Default, Debug)]
@@ -42,84 +69,6 @@ impl Todo {
             content: content.to_string(),
             completed,
         }
-    }
-
-    fn file() -> std::io::Result<File> {
-        OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(FILENAME)
-    }
-
-    fn read() -> std::io::Result<String> {
-        let mut contents = String::new();
-        Self::file()?.read_to_string(&mut contents)?;
-        return Ok(contents);
-    }
-
-    fn parse(contents: String) -> Vec<Self> {
-        let mut todos: Vec<Todo> = vec![];
-        for line in contents.lines() {
-            if let Some(todo) = Self::parse_line(line.to_string()) {
-                todos.push(todo)
-            }
-        }
-        return todos;
-    }
-
-    pub fn parse_line(line: String) -> Option<Self> {
-        if let Some(content) = line.strip_prefix("- [] ") {
-            Some(Todo::new(content, false))
-        } else if let Some(content) = line.strip_prefix("- [x] ") {
-            Some(Todo::new(content, true))
-        } else {
-            None
-        }
-    }
-
-    // this will call read
-    // and parse each new line into todo
-    fn all() -> Vec<Self> {
-        match Self::read() {
-            Ok(contents) => Self::parse(contents),
-            Err(_) => todo!(),
-        }
-    }
-
-    fn add(content: String, completed: bool) -> std::io::Result<Vec<Todo>> {
-        let mut todos = Todo::all();
-        let todo = Todo::new(&content, completed);
-        todos.push(todo);
-        Todo::write(&todos)?;
-        return Ok(todos);
-    }
-
-    fn update_by(index: usize, completed: bool) -> std::io::Result<Vec<Todo>> {
-        let mut todos = Todo::all();
-        let mut todo = &mut todos[index];
-        todo.completed = completed;
-        Todo::write(&mut todos)?;
-        return Ok(todos);
-    }
-
-    fn delete_by(index: usize) -> std::io::Result<Vec<Todo>> {
-        let mut todos = Todo::all();
-        todos.remove(index);
-        Todo::write(&todos)?;
-        return Ok(todos);
-    }
-
-    fn write(todos: &Vec<Todo>) -> std::io::Result<()> {
-        let contents = todos
-            .iter()
-            .map(|todo| todo.to_s())
-            .collect::<Vec<String>>()
-            .join("\n");
-        let mut f = Self::file()?;
-        f.set_len(0)?;
-        f.write_all(contents.as_bytes())?;
-        Ok(())
     }
 
     fn to_s(&self) -> String {
@@ -137,72 +86,124 @@ impl Display for Todo {
     }
 }
 
-fn list() {
-    let todos = Todo::all();
-    let mut x = 0;
-    for todo in todos {
-        println!("{} {}", x, todo);
-        x += 1;
+struct App {
+    args: Vec<String>,
+    contents: String,
+}
+
+impl App {
+    fn todos(&self) -> Vec<Todo> {
+        let mut todos: Vec<Todo> = vec![];
+        for line in self.contents.lines() {
+            if let Some(todo) = Self::todo(line.to_string()) {
+                todos.push(todo)
+            }
+        }
+        return todos;
     }
-}
 
-fn add(args: Vec<String>) -> std::io::Result<Vec<Todo>> {
-    Todo::add(args.join(" "), false)
-}
+    pub fn todo(line: String) -> Option<Todo> {
+        if let Some(content) = line.strip_prefix("- [] ") {
+            Some(Todo::new(content, false))
+        } else if let Some(content) = line.strip_prefix("- [x] ") {
+            Some(Todo::new(content, true))
+        } else {
+            None
+        }
+    }
 
-fn done(index: usize) -> std::io::Result<Vec<Todo>> {
-    Todo::update_by(index, true)
-}
+    fn list(self) -> Result<String, std::io::Error> {
+        let todos = self.todos();
+        let mut x = 0;
+        let mut output = vec![];
+        for todo in todos {
+            output.push(format!("{} {}", x, todo));
+            x += 1;
+        }
+        return Ok(output.join("\n"));
+    }
 
-fn delete(index: usize) -> std::io::Result<Vec<Todo>> {
-    Todo::delete_by(index)
-}
+    fn add(&self) -> Vec<Todo> {
+        let content: String = self
+            .args
+            .clone()
+            .into_iter()
+            .skip(1)
+            .collect::<Vec<String>>()
+            .join(" ");
+        let mut todos = self.todos();
+        todos.push(Todo::new(&content, false));
+        return todos;
+    }
 
-fn undo(index: usize) -> std::io::Result<Vec<Todo>> {
-    Todo::update_by(index, false)
+    fn command(&self) -> Action {
+        if self.args.len() > 0 {
+            match self.args[0].parse() {
+                Ok(action) => action,
+                Err(_) => Action::Help,
+            }
+        } else {
+            Action::List
+        }
+    }
+
+    fn help(&self) -> String {
+        String::from("Try add, done, delete or undo")
+    }
+
+    fn done(&self) -> Vec<Todo> {
+        self.update(true)
+    }
+
+    fn index(&self) -> usize {
+        if let Some(idx) = self.args.get(1) {
+            return idx.parse().unwrap_or(0);
+        } else {
+            return 0;
+        }
+    }
+
+    fn update(&self, completed: bool) -> Vec<Todo> {
+        let mut todos = self.todos();
+        if let Some(mut todo) = todos.get_mut(self.index()) {
+            todo.completed = completed;
+        }
+        return todos;
+    }
+
+    fn delete(&self) -> Vec<Todo> {
+        let mut todos = self.todos();
+        let index = self.index();
+        if let Some(_) = todos.get_mut(index) {
+            todos.remove(index);
+        }
+        return todos;
+    }
+
+    fn undo(&self) -> Vec<Todo> {
+        self.update(false)
+    }
 }
 
 fn main() -> std::io::Result<()> {
+    // get the input to the program
+    // call it state, why not
     let args: Vec<String> = env::args().skip(1).collect();
-    if args.len() > 0 {
-        if let Ok(action) = args[0].parse() {
-            match action {
-                Action::List => {
-                    list();
-                }
-                Action::Add => {
-                    add(args.into_iter().skip(1).collect::<Vec<String>>())?;
-                    list();
-                }
-                Action::Done => {
-                    if args.len() > 1 {
-                        let idx: usize = args[1].parse().unwrap();
-                        done(idx)?;
-                        list();
-                    }
-                }
-                Action::Delete => {
-                    if args.len() > 1 {
-                        let idx: usize = args[1].parse().unwrap();
-                        delete(idx)?;
-                        list();
-                    }
-                }
-                Action::Undo => {
-                    if args.len() > 1 {
-                        let idx: usize = args[1].parse().unwrap();
-                        undo(idx)?;
-                        list();
-                    }
-                }
-            }
-        } else {
-            println!("Try add, list, done, del or undo")
-        }
-    } else {
-        list();
-    }
-    Ok(())
+    let contents: String = read_file()?;
+    let app = App { args, contents };
+
+    let output = match app.command() {
+        Action::List => app.list()?,
+        Action::Add => write_file(app.add())?,
+        Action::Done => write_file(app.done())?,
+        Action::Delete => write_file(app.delete())?,
+        Action::Undo => write_file(app.undo())?,
+        Action::Help => app.help(),
+    };
+
+    println!("{}", output);
+
+    return Ok(());
 }
 
 #[cfg(test)]
@@ -211,7 +212,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        if let Some(todo) = Todo::parse_line(String::from("- [] hello")) {
+        if let Some(todo) = App::todo(String::from("- [] hello")) {
             assert_eq!(todo.content, String::from("hello"));
             assert_eq!(todo.completed, false);
         }
@@ -219,7 +220,7 @@ mod tests {
 
     #[test]
     fn it_works_when_completed() {
-        if let Some(todo) = Todo::parse_line(String::from("- [x] hello")) {
+        if let Some(todo) = App::todo(String::from("- [x] hello")) {
             assert_eq!(todo.content, String::from("hello"));
             assert_eq!(todo.completed, true);
         }
